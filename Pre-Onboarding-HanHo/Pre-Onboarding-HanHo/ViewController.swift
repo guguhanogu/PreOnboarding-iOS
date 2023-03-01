aed86c18fc795e8dccb808a7a4ca5b64cf0f39fb
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-03-01 18:32:07 +0900

    [Init] Project 생성
    초기 프로젝트를 생성합니다.

---
+++ Pre-Onboarding-HanHo/Pre-Onboarding-HanHo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pre-Onboarding-HanHo
+//
+//  Created by 최한호 on 2023/03/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
