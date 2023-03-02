8da56ee2e6663ee4caa95107dffcd78a8a66a1b7
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-03-02 22:00:23 +0900

    [Add] #3 커스텀 버튼 생성
    # 커스텀 버튼 생성
    - RoundButton.swift

---
+++ Pre-Onboarding-HanHo/Pre-Onboarding-HanHo/RoundButton.swift
@@ -0,0 +1,16 @@
+//
+//  RoundButton.swift
+//  Pre-Onboarding-HanHo
+//
+//  Created by 최한호 on 2023/03/02.
+//
+
+import UIKit
+
+public class RoundButton: UIButton {
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = layer.frame.height / 2
+    }
+}
